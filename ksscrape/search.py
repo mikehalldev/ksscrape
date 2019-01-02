4345bb22d43bbd4c423d81a6a8717bea4b76c0f6
televisedprogram <mikehall.dev@gmail.com>
2019-01-01 21:20:17 -0500

    Added documentation/clarification of methods

--- ksscrape/search.py
+++ ksscrape/search.py
@@ -17,14 +17,27 @@ DROPDOWN_CONTAINER_XPATH = '//div[@aria-hidden="false" and @id="{}"]'
 class DiscoverySearcher(object):
     """
     Searches the Discover page to find links to projects based on given criteria.
+
+    Attributes:
+        driver: Selenium webdriver object
     """
 
     def __init__(self, driver):
         self.driver = driver
-        self.got_count = 0
-        self.wait = WebDriverWait(self.driver, 10)
+        self._wait = WebDriverWait(self.driver, 10)
 
     def get_search_page(self, category, sort_by, sub_category=None):
+        """
+        Gets the Discovery page for the given arguments
+
+        Arguments:
+            category: the category to choose from (must match exactly to category dropdown)
+            sort_by: how to search the category (must match exactly from sort by dropdown)
+            sub_category: category to choose after category is chosen (must match exactly from sub category dropdown)
+
+        Returns:
+            None
+        """
         self.driver.get('https://kickstarter.com/discover')
 
         time.sleep(3)
@@ -73,28 +86,28 @@ class DiscoverySearcher(object):
         self._category_dropdown_wait(dropdown_id)
 
     def _url_change_wait(self):
-        self.wait.until(
+        self._wait.until(
             EC.url_changes(self.driver.current_url)
         )
 
     def _category_dropdown_wait(self, dropdown_id):
-        self.wait.until(
+        self._wait.until(
             EC.presence_of_element_located((
                 By.XPATH,
                 DROPDOWN_CONTAINER_XPATH.format(dropdown_id)
             ))
         )
 
-    def click_load_more(self):
+    def _click_load_more(self):
         """Loads more projects"""
         self.driver.find_element_by_xpath(
             ANCHOR_TAG_BUTTON.format('Load more')
             ).click()
     
     def scroll_until_count(self, count, min_sleep=2, max_sleep=8):
-        """Scrolls until 'count' is in view"""
+        """Scrolls until 'count' is in view. Sleeps between min/max sleep times"""
         while len(self.get_project_card_elements()) < count:
-            self.click_load_more()
+            self._click_load_more()
             time.sleep(
                 random.randint(min_sleep, max_sleep) + random.uniform(0, 1))
 
@@ -102,7 +115,7 @@ class DiscoverySearcher(object):
         return self.driver.find_elements_by_class_name('js-react-async-proj-card')
 
     def scroll_until_all(self, min_sleep=2, max_sleep=8):
-        """Scrolls until all projects are in view"""
+        """Scrolls until all projects are in view. Sleeps between min/max sleep times"""
         self.scroll_until_count(self._get_project_count(), min_sleep, max_sleep)
 
     def _get_project_count(self):
@@ -110,7 +123,7 @@ class DiscoverySearcher(object):
         count_digits = [char for char in elem.text if char.isdigit()]
         return int(''.join(count_digits))
 
-    def get_projects_dictionaries(self):
+    def get_cards_dictionaries(self):
         """Gets project data as list of dictionaries"""
         cards = self.get_project_card_elements()
         projects = []
