c08637c4a16805be4da48a20a6359b23a896d94e
televisedprogram <mikehall.dev@gmail.com>
2019-02-22 21:20:20 -0500

    fix: Add ProjectScraper to scrape project information

--- ksscrape/project.py
+++ ksscrape/project.py
@@ -2,18 +2,32 @@ import json
 from bs4 import BeautifulSoup
 
 
-def get_project_dictionary(html, markup='lxml'):
-    """
-    Finds the data-initial attribute of the project and converts it into a dictionary object.
+class ProjectScraper(object):
+    
+    def __init__(self, html, markup='lxml'):
+        self.html = html
+        self.soup = BeautifulSoup(html, markup)
 
-    Attributes:
-        html: The HTML of the kickstarter project as a string
-        markup: the markup type to use with bs4 (default='lxml')
 
-    Returns:
-        a dictionary of the data presented in the data-initial attribute.
-    """
-    soup = BeautifulSoup(html, markup)
-    elem = soup.find('div', attrs={'data-initial': True})
+    def get_project_dictionary(self):
+        """
+        Finds the data-initial attribute of the project and converts it into a dictionary object.
 
-    return json.loads(elem['data-initial'])
+        Attributes:
+            html: The HTML of the kickstarter project as a string
+            markup: the markup type to use with bs4 (default='lxml')
+
+        Returns:
+            a dictionary of the data presented in the data-initial attribute.
+        """
+        elem = self.soup.find('div', attrs={'data-initial': True})
+        return json.loads(elem['data-initial'])
+
+    def get_external_creator_urls(self):
+        project = self.get_project_dictionary()
+
+        urls = []
+        for website_info in project['project']['creator']['websites']:
+            urls.append(website_info[['url']])
+
+        return urls
